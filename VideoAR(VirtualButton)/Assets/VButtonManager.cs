82751066bbcfade23f100400cc8c7d924de7be3c
Naveenasri02 <66512636+Naveenasri02@users.noreply.github.com>
2020-11-04 21:17:24 +0530

    first commit

---
+++ VideoAR(VirtualButton)/Assets/VButtonManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.Video;
+public class VButtonManager : MonoBehaviour,IVirtualButtonEventHandler
+{
+    public VideoPlayer player;
+    public GameObject playButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        player.Play();
+        playButton.GetComponent<Renderer>().enabled = false;
+    }
+
+    void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        player.Pause();
+        playButton.GetComponent<Renderer>().enabled = true;
+    }
+
+   
+}
